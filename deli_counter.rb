24dbae4753b7855bea0a2c102188d266b917776f
Sam Bryant <bryantsj@gmail.com>
2020-02-16 20:31:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 katz_deli = []
 
 def line(katz_deli)
+  new_array = []
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    new_array = []
     counter = 0
     katz_deli.each do |x|
       new_array = "#{counter}. #{x}"
